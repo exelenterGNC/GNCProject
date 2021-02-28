2d91d1cb2fca9c9b625c8e416950774261db965d
Furkat <furkat.khalilov@gmail.com>
2021-02-27 22:34:13 -0500

    Path has been created

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -8,6 +8,15 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import utils.BaseClass;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 public class Hooks {
 
     // With hook class I am telling to program to start and to finish
@@ -17,6 +26,9 @@ public class Hooks {
     @Before
     public void start(){
         driver = BaseClass.setUp();
+        driver.get(readProperties("gnc.url"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS) ;
     }
 
     @After
@@ -30,6 +42,17 @@ public class Hooks {
 //        BaseClass.tearDown();
     }
 
+    private String readProperties(String key) {
+        Properties prop = new Properties();
+        String propValue = "";
+        URL resourcePath = Hooks.class.getClassLoader().getResource("config.properties");
+        System.out.println("resourcePath = " + resourcePath);
 
-
+        try(InputStream input = new FileInputStream(resourcePath.getPath())){
+            prop.load(input);
+            propValue =  prop.getProperty(key);
+        }catch(IOException ex){
+        }
+        return propValue;
+    }
 }
